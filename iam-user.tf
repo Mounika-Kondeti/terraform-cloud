6ab745784cc98d8d7880cac8010614e913ecad01
mounikakondeti <149581172+Mounika-Kondeti@users.noreply.github.com>
2024-02-05 15:29:12 +0530

    Update iam-user.tf

--- iam-user.tf
+++ iam-user.tf
@@ -12,3 +12,15 @@ resource "aws_iam_user" "testtf2" {
 }
 
 
+resource "aws_iam_user" "testtf3" {
+  name = "testtf2"
+  
+
+  tags = {
+    application        = "contido"
+    product            = "contido_webapp"
+    environment        = "${var.env}"
+    client             = "${var.client}"
+    iac                = "terraform"
+  }
+}
